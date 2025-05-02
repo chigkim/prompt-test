6b2585df8026e444878bcc7408ce89ea434c3896
Streamlit <streamlit@localhost>
2025-05-02 16:57:41 -0400

    OpenAI API

---
+++ test-oai.py
@@ -0,0 +1,186 @@
+base_url = "http://localhost:8080/v1"
+api_key = "api_key"
+model = "qwen3:30b-a3b-q8_0"
+model = "mlx-community/Qwen3-30B-A3B-8bit"
+model = "Qwen/Qwen3-30B-A3B-FP8"
+max_tokens = 2000
+temperature = 0.7
+top_p = 0.8
+seed = 1000
+prompt_file = "prompt.txt"
+system_prompts = [
+    "You are a helpful assistant. Provide a summary as well as a detail analysis of the following. /no_think",
+    "Provide a summary as well as a detail analysis of the following. You are a helpful assistant. /no_think",
+]
+cooling = 120
+headers = [
+    "Machine",
+    "Engine",
+    "Prompt Tokens",
+    "Prompt Processing Speed",
+    "Time to First Token",
+    "Generated Tokens",
+    "Token Generation Speed",
+    "Total Duration",
+]
+values = ["M3Max", "MLX"]
+
+import time
+
+start = time.time()
+from datetime import datetime, timedelta
+import os
+from glob import glob
+import openai
+import codecs
+import re
+import math
+import shutil
+
+
+def generate_prompts(file, initial_ratio=0.02):
+    text = codecs.open(file, "r", "utf-8").read()
+    words = re.findall(r"\S+\s*", text)
+    phi = (1 + math.sqrt(5)) / 2
+    ratio = 1 / (1 + phi)
+    prompts = []
+    total = len(words)
+    take = int(total * initial_ratio)
+
+    while words:
+        if take > len(words):
+            prompts[-1] = words + prompts[-1]
+            break
+        take = take
+        if prompts:
+            prompt = words[-take:] + prompts[-1]
+        else:
+            prompt = words[-take:]
+        prompts.append(prompt)
+        words = words[:-take]
+        take = int(len(prompt) * ratio)
+    prompts = ["".join(prompt) for prompt in prompts]
+    print(f"Generated {len(prompts)} prompts.")
+    return prompts
+
+
+def send(messages, change_system_prompt=True):
+    if change_system_prompt:
+        # Flip system prompt to avoid prompt caching.
+        messages[0]["content"] = (
+            system_prompts[0]
+            if messages[0]["content"] != system_prompts[0]
+            else system_prompts[1]
+        )
+    ttf = 0
+    start_time = time.time()
+    stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        top_p=top_p,
+        max_tokens=max_tokens,
+        seed=seed,
+        stream=True,
+        stream_options={"include_usage": True},
+    )
+    for event in stream:
+        if ttf == 0:
+            ttf = time.time() - start_time
+    duration = time.time() - start_time
+    prompt_tokens = event.usage.prompt_tokens
+    prompt_speed = prompt_tokens / ttf
+    completion_tokens = event.usage.completion_tokens
+    completion_speed = completion_tokens / (duration - ttf)
+    return (
+        prompt_tokens,
+        prompt_speed,
+        ttf,
+        completion_tokens,
+        completion_speed,
+        duration,
+    )
+
+
+def report(
+    prompt_tokens,
+    prompt_speed,
+    ttf,
+    completion_tokens,
+    completion_speed,
+    duration,
+    write_log=True,
+):
+    res = values.copy()
+    res.append(f"{prompt_tokens}")
+    res.append(f"{prompt_speed:.2f}")
+    res.append(f"{ttf:.2f}")
+    res.append(f"{completion_tokens}")
+    res.append(f"{completion_speed:.2f}")
+    res.append(f"{duration:.2f}")
+    msg = "| " + " | ".join(res) + " |"
+    print(msg)
+    if write_log:
+        log(msg)
+
+
+def elapsed(start):
+    duration = time.time() - start
+    duration_td = timedelta(seconds=duration)
+    days = duration_td.days
+    hours, remainder = divmod(duration_td.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    dur_str = ""
+    if days:
+        dur_str = f"{days}d"
+    if hours:
+        dur_str += f"{hours}h"
+    if minutes:
+        dur_str += f"{minutes}m"
+    if seconds:
+        dur_str += f"{seconds}s"
+    return dur_str
+
+
+def log(msg):
+    with open(report_file, "a") as file:
+        file.write(msg + "\n")
+
+
+client = openai.Client(base_url=base_url, api_key=api_key)
+model_name = re.sub(r"\W", "_", model)
+report_file = f"report-{model_name}.txt"
+try:
+    os.remove(report_file)
+except:
+    pass
+log(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+log(f"Model: {model_name}")
+
+headers_text = "| " + " | ".join(headers) + " |"
+log(headers_text)
+header_line = [re.sub(r".", "-", header) for header in headers]
+header_line = "| " + " | ".join(header_line) + " |"
+log(header_line)
+
+prompts = generate_prompts(prompt_file)
+messages = [
+    {"role": "system", "content": "Act as a system admin."},
+    {"role": "user", "content": "This is test."},
+]
+print("Sending a warm up test.")
+send(messages, change_system_prompt=False)
+
+for i, prompt in enumerate(prompts):
+    print("Sending prompt ", i + 1)
+    messages[-1]["content"] = prompt
+    stats = send(messages)
+    report(*stats)
+    if cooling:
+        print(f"Coolling down for {cooling} seconds.")
+        time.sleep(cooling)
+
+
+msg = f"\nTotal duration: {elapsed(start)}"
+print(msg)
+log(msg)
--- test.py
+++ test.py
@@ -2,9 +2,10 @@ llama_cli_path = "../llama.cpp/build/bin/llama-cli"
 model_path = "../models/Llama-3.2-1B-Instruct-Q4_K_M.gguf"
 parameters = "--n-predict 2000 --temp 0.0 --top_p 0.9 --seed 1000 --batch-size 4096 --ubatch-size 4096 --flash-attn"
 # For Cuda, You may need to assign buff_context to same as --n-predict.
-buffer_context_size = 0 
+buffer_context_size = 0
 
 import time
+
 start = time.time()
 import subprocess
 import shlex
@@ -13,43 +14,47 @@ from datetime import datetime, timedelta
 import os
 from glob import glob
 
+
 def atoi(text):
-	return int(text) if text.isdigit() else text
+    return int(text) if text.isdigit() else text
+
 
 def natural_keys(text):
-	return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+    return [atoi(c) for c in re.split(r"(\d+)", text)]
+
 
 def elapsed(start):
-	duration = time.time() - start
-	duration_td = timedelta(seconds=duration)
-	days = duration_td.days
-	hours, remainder = divmod(duration_td.seconds, 3600)
-	minutes, seconds = divmod(remainder, 60)
-	dur_str = ""
-	if days:
-		dur_str = f"{days}d"
-	if hours:
-		dur_str += f"{hours}h"
-	if minutes:
-		dur_str += f"{minutes}m"
-	if seconds:
-		dur_str += f"{seconds}s"
-	return dur_str
+    duration = time.time() - start
+    duration_td = timedelta(seconds=duration)
+    days = duration_td.days
+    hours, remainder = divmod(duration_td.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    dur_str = ""
+    if days:
+        dur_str = f"{days}d"
+    if hours:
+        dur_str += f"{hours}h"
+    if minutes:
+        dur_str += f"{minutes}m"
+    if seconds:
+        dur_str += f"{seconds}s"
+    return dur_str
+
 
 def log(msg):
-	with open(report_file, "a") as file:
-		file.write(msg+"\n")
-	
-	
+    with open(report_file, "a") as file:
+        file.write(msg + "\n")
+
+
 def run(command):
-	print(command)
-	command_args = shlex.split(command)
-	start = time.time()
-	process = subprocess.Popen(command_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-	stdout, stderr = process.communicate()
-	return stderr, elapsed(start)
-
-
+    print(command)
+    command_args = shlex.split(command)
+    start = time.time()
+    process = subprocess.Popen(
+        command_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+    stdout, stderr = process.communicate()
+    return stderr, elapsed(start)
 
 
 r = r"(\d+\.\d+) tokens per second"
@@ -60,37 +65,41 @@ out, dur = run(cmd)
 build = re.search(r"build: (.*?) with", out)[1]
 model_name = os.path.basename(model_path)
 report_file = f"report-{model_name} b{build}.txt"
-try: os.remove(report_file)
-except: pass
+try:
+    os.remove(report_file)
+except:
+    pass
 log(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 log(f"Model: {model_name}")
 log(f"Build: {build}\n")
-log("| Prompt Tokens | Prompt Processing Speed | Generated Tokens | Token Generation Speed | Total Execution Time |")
+log(
+    "| Prompt Tokens | Prompt Processing Speed | Generated Tokens | Token Generation Speed | Total Execution Time |"
+)
 log("| --- | --- | --- | --- | --- |")
 
 init_out = ""
 for file in files:
-	count = int(re.search(r"(\d+)", file)[1])
-	cmd = f"{llama_cli_path} -m {model_path} --ctx-size {count+buffer_context_size} {parameters} --file '{file}'"
-	out, dur = run(cmd)
-	speeds = re.findall(r, out)
-	for line in out.split("\n"):
-		if not init_out:
-			init_out = out
-			print(out)
-			break
-		if line not in init_out:
-			print(line)
-	runs = re.findall(r" (\d+) runs", out)
-	tokens = re.findall(r" (\d+) tokens", out)
-	res = [tokens[-2]]
-	res.append(speeds[1])
-	res.append(runs[-1])
-	res.append(speeds[2])
-	res.append(dur)
-	msg = "| "+" | ".join(res)+" |"
-	print(msg)
-	log(msg)
+    count = int(re.search(r"(\d+)", file)[1])
+    cmd = f"{llama_cli_path} -m {model_path} --ctx-size {count+buffer_context_size} {parameters} --file '{file}'"
+    out, dur = run(cmd)
+    speeds = re.findall(r, out)
+    for line in out.split("\n"):
+        if not init_out:
+            init_out = out
+            print(out)
+            break
+        if line not in init_out:
+            print(line)
+    runs = re.findall(r" (\d+) runs", out)
+    tokens = re.findall(r" (\d+) tokens", out)
+    res = [tokens[-2]]
+    res.append(speeds[1])
+    res.append(runs[-1])
+    res.append(speeds[2])
+    res.append(dur)
+    msg = "| " + " | ".join(res) + " |"
+    print(msg)
+    log(msg)
 
 msg = f"\nTotal duration: {elapsed(start)}"
 print(msg)
